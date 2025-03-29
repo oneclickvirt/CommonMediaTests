592d1de943bd70baa6ed6a98dd11e4029d0f7aba
spiritlhl <103393591+spiritLHLS@users.noreply.github.com>
2025-03-29 16:03:30 +0800

    Update main.go

--- cmd/main.go
+++ cmd/main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	go func() {
-		http.Get("https://hits.seeyoufarm.com/api/count/incr/badge.svg?url=https%3A%2F%2Fgithub.com%2Foneclickvirt%2FCommonMediaTests&count_bg=%2379C83D&title_bg=%23555555&icon=&icon_color=%23E7E7E7&title=hits&edge_flat=false")
+		http.Get("https://hits.spiritlhl.net/CommonMediaTests.svg?action=hit&title=Hits&title_bg=%23555555&count_bg=%230eecf8&edge_flat=false")
 	}()
 	fmt.Println(Green("项目地址:"), Yellow("https://github.com/oneclickvirt/CommonMediaTests"))
 	var showVersion, help bool
